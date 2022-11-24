29a8277d8d16c75d59839c527e22365b378bc04f
Ivan <soloveii@mydigicode.com>
2022-11-25 00:11:48 +0200

    added cors and morgan

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const positionRoutes = require("./routes/position");
 const app = express();
 
 
+app.use(require("morgan")("dev"))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
+app.use(require("cors")())
+
 
 app.use('/api/auth', authRoutes)
 app.use('/api/analytics', analyticsRoutes)
