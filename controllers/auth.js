f9674e740dadd61a1990555243a3c83d22a24007
Ivan <soloveii@mydigicode.com>
2022-12-12 21:23:23 +0200

    added login functionality, not finished

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,28 @@
 const { user } = require('../config/connection-keys');
 const bcrypt = require('bcryptjs')
-const User = require('../models/User')
+const jsw = require('jsonwebtoken')
+const User = require('../models/User');
+const { token } = require('morgan');
 
-module.exports.login = function(req, res) {
-    return res.status(200).json({
-       login: {
+module.exports.login = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email})
+   
+    if(candidate) {
+       const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+
+       if(passwordResult) {
+          const token = jsw.sign({
             email: req.body.email,
             password: req.body.password
+          }, process.env.jsw, {expiresIn: 60*60})
+          res.status(200).json({
+            message: 'Successful login',
+            token: token,
+           })
        }
-    });
+    } else {
+        res.status(401).json('User with current email not found. Try again.')
+    }
 }
 
 module.exports.register = async function(req, res) {
@@ -26,7 +40,7 @@ module.exports.register = async function(req, res) {
             email: req.body.email,
             password: bcrypt.hashSync(password, salt)
             })
-            
+
         try {
           await user.save()
           res.status(201).json(user)
